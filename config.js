31d33f5817e89b2d1b38a0e0d721fc1bab8d6347
abufrank <riley.thornton1@gmail.com>
2020-11-12 18:21:48 -0500

    added secret key variable

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   MONGODB: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@chatter.1vk2o.mongodb.net/chatter?retryWrites=true&w=majority`,
-  SECRET_KEY: "93247iujdfjewou",
+  SECRET_KEY: `${process.env.SECRET_KEY}`,
 };
