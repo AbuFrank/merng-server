d1cc91c2451b223dba6513905a9f59b93c27b122
abufrank <riley.thornton1@gmail.com>
2020-11-12 12:54:23 -0500

    fixed mongodb auth

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB:
-    "mongodb+srv://contact-first-social:kKcQ8lAnjMYqqhrF@chatter.1vk2o.mongodb.net/chatter?retryWrites=true&w=majority",
+    "mongodb+srv://first-stack-social:8RMkcpVYhqrGZPj8@chatter.1vk2o.mongodb.net/chatter?retryWrites=true&w=majority",
   SECRET_KEY: "93247iujdfjewou",
 };
