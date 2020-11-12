1f6608b29c3a5bb05df51b36ecb8f5b390b09b2e
abufrank <riley.thornton1@gmail.com>
2020-11-12 18:18:25 -0500

    added server config variables

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  MONGODB:
-    "mongodb+srv://first-stack-social:8RMkcpVYhqrGZPj8@chatter.1vk2o.mongodb.net/chatter?retryWrites=true&w=majority",
+  MONGODB: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@chatter.1vk2o.mongodb.net/chatter?retryWrites=true&w=majority`,
   SECRET_KEY: "93247iujdfjewou",
 };
