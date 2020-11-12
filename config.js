48823dca906cc5a52b9a2d7bab94457453f331f8
abufrank <riley.thornton1@gmail.com>
2020-11-12 12:07:54 -0500

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://contact-first-social:kKcQ8lAnjMYqqhrF@chatter.1vk2o.mongodb.net/chatter?retryWrites=true&w=majority",
+  SECRET_KEY: "93247iujdfjewou",
+};
