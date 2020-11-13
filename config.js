7789fb108979683220adb2317f7d0c449fece06b
abufrank <riley.thornton1@gmail.com>
2020-11-12 23:08:07 -0500

    replaced mongodb uri with env var

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@chatter.1vk2o.mongodb.net/chatter?retryWrites=true&w=majority`,
+  MONGODB: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}?retryWrites=true&w=majority`,
   SECRET_KEY: `${process.env.SECRET_KEY}`,
 };
